64e79a37f280613aecb2b2d20c28095a20f17651
arif-rizal1122 <rizallarif32@gmail.com>
2025-09-10 16:56:48 +0800

    tipe data string

---
+++ src/main/tipe_data_string.kt
@@ -0,0 +1,14 @@
+package main
+
+fun main() {
+
+    var string = """saya adalah orang yang baik, dan tidak sombong!! HAHAHAHH jjjjjjjjjj
+        | iedjiejdiejdi
+        | wojdeijdiejfidjei
+        | doejifjeiwjfiejf
+        | dewduwefhuwehfuywer
+    """.trimMargin()
+
+    println(string)
+
+}
